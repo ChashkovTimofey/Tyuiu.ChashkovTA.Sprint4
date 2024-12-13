2098e0d83bce710c36513e2192a6669e11b90fd7
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 16:28:22 +0500

    Task7

---
+++ Tyuiu.ChashkovTA.Sprint4.Task7.V5.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ChashkovTA.Sprint4.Task7.V5.Lib
+{
+    public class DataService : ISprint4Task7V5
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            if (value.Length != n * m)
+            {
+                throw new ArgumentException("Длина строки не соответствует размеру матрицы.");
+            }
+
+            int[,] matrix = new int[n, m];
+            int evenCount = 0;
+
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(value[index].ToString());
+                    if (matrix[i, j] % 2 == 0) 
+                    {
+                        evenCount++; 
+                    }
+                    index++;
+                }
+            }
+
+            return evenCount;
+        }
+    }
+}
---
+++ Tyuiu.ChashkovTA.Sprint4.Task7.V5/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ChashkovTA.Sprint4.Task7.V5.Lib;
+namespace Tyuiu.ChashkovTA.Sprint4.Task7.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #5                                                            *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
