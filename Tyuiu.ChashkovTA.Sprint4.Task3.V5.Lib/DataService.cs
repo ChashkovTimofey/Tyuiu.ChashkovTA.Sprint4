2d5912392cfaf19bfaeedd9a4e4cf99cde0b8c39
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 16:14:37 +0500

    Task3

---
+++ Tyuiu.ChashkovTA.Sprint4.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ChashkovTA.Sprint4.Task3.V5.Lib
+{
+    public class DataService : ISprint4Task3V5
+    {
+        public int Calculate(int[,] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum += array[i, 0]; // Суммирование элементов первого столбца
+            }
+            return sum;
+        }
+    }
+    
+}
---
+++ Tyuiu.ChashkovTA.Sprint4.Task3.V5/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.ChashkovTA.Sprint4.Task3.V5.Lib;
+namespace Tyuiu.ChashkovTA.Sprint4.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                            *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+            int[,] array = {
+            { 3, 4, 8, 6, 3 },
+            { 9, 9, 3, 6, 9 },
+            { 5, 6, 7, 2, 3 },
+            { 3, 6, 2, 3, 8 },
+            { 5, 7, 2, 6, 4 }
+        };
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(array);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
