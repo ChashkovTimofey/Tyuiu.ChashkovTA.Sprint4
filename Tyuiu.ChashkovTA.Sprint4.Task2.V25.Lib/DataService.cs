f3c61347d6034bd512c5b3f66249c9c926aa33b2
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 16:08:09 +0500

    Task2

---
+++ Tyuiu.ChashkovTA.Sprint4.Task2.V25.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ChashkovTA.Sprint4.Task2.V25.Lib
+{
+    public class DataService : ISprint4Task2V25
+    {
+        public int Calculate(int[] array)
+        {
+            int product = 1;
+            bool hasEven = false; // Флаг для проверки наличия четных чисел
+
+            foreach (int num in array)
+            {
+                if (num % 2 == 0) // Проверка на четность
+                {
+                    product *= num;
+                    hasEven = true;
+                }
+            }
+
+            return hasEven ? product : 0; // Возврат произведения или 0, если четных чисел нет
+        }
+    }
+}
